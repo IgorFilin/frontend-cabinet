4562f0d09a65f73b41294c9625e221814d155aaf
IgorFilin <filinigor@yandex.ru>
2025-12-03 01:11:17 +0300

    доработки конфига

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,12 @@
 module.exports = function (config) {
   config.set({
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -7,6 +14,14 @@ module.exports = function (config) {
         flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
+    reporters: ['progress', 'junit'],
+    junitReporter: {
+      outputFile: 'test-results.xml',
+      sendToStdout: false
+    },
     singleRun: true,
+    autoWatch: false,
+    colors: true,
+    logLevel: config.LOG_INFO
   });
 };

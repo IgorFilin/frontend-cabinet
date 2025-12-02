b44f6bfc8f7b9e7a03b04df662ed8032c34299ce
IgorFilin <filinigor@yandex.ru>
2025-12-02 03:32:44 +0300

    Обновление конфига для CI тестов

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,36 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     client: {
       jasmine: {
         random: false
+      },
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    reporters: ['progress', 'junit'],
+    junitReporter: {
+      outputDir: '.',
+      outputFile: 'test-results.xml',
+      useBrowserName: false
+    },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--remote-debugging-port=9222']
       }
     },
+    browsers: ['ChromeHeadlessCI'],
+    singleRun: true,
+    restartOnFileChange: false
   });
 };

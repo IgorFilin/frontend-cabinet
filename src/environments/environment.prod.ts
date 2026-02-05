64e1e160ed5108d6da60034e48db6739323cebfd
IgorFilin <filinigor@yandex.ru>
2026-02-05 16:01:43 +0300

    Обновление домена

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://training-program.ru/api',
-  chatBaseUrl: 'wss://training-program.ru/chat/ws',
-  teminalUrl: 'wss://training-program.ru/term/ws',
+  apiBaseUrl: 'https://kiber-hub.ru/api',
+  chatBaseUrl: 'wss://kiber-hub.ru/chat/ws',
+  teminalUrl: 'wss://kiber-hub.ru/term/ws',
   auth_vk: {
     app: 52897712,
-    redirectUrl: 'https://training-program.ru/',
+    redirectUrl: 'https://kiber-hub.ru/',
   },
 };
